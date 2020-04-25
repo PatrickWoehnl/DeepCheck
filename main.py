25099364014efe7d32e218914db82fa9be708d28
patrick.woehnl <patrick.weohnl@gmail.com>
2020-04-25 18:10:09 +0200

    Main angepasst

--- main.py
+++ main.py
@@ -1,52 +1,32 @@
 from newspaper import Article
-import csv
+
 import newspaper
 
-from flask import Flask, jsonify
 
-app = Flask(__name__)
+def GetArticleText(event, context):
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+    url = event['text']
 
-@app.route('/deepchek/api/v1.0/CheckArticle/<string:url>', methods=['GET'])
-def CheckArticle(url):
-    if url.startswith('http'):
+    if url.startswith(('http', 'www.')):
         url = url
         article = Article(url)
         article.download()
         article.parse()       
-    return jsonify({'CheckArticle': article.text})
+    # text weiter verarbeiten
+        return article.text
+    return url
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
-
+def Test():
     url = 'https://nbpostgazette.com/florida-woman-dies-of-flesh-eating-bacteria-while-after-stumbling-on-beach/'
-    article = Article(url)
-    article.download()
-    article.parse()
-    print(article.text)
+    if url.startswith(('http', 'www.')):
+        url = url
+        article = Article(url)
+        article.download()
+        article.parse() 
+        print(article.text)
 
-    with open('D:\\aa git\\newspaper\\names.csv', 'w', newline='') as csvfile:
-        fieldnames = ['text', 'label']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+if __name__ == '__main__':
+    Test()
+    
 
-        writer.writeheader()
-        writer.writerow({'text': article.text, 'label': 0})
-        writer.writerow({'text': 'Lovely', 'label': 'Spam'})
-        writer.writerow({'text': 'Wonderful', 'label': 'Spam'})
-        
+    
